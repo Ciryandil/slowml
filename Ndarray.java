77534414590b7150c3b70e79c1ee3f69358b3bd6
Ciryandil <soham.ganguly12@gmail.com>
2020-08-28 20:57:13 +0530

    added get(), put(), reshape(), and find_index() to Ndarray

--- Ndarray.java
+++ Ndarray.java
@@ -5,15 +5,21 @@ class Ndarray
     private double arr[];
     public int num_inds;
     public int shape[];
-    
+    private int multipliers[];
     public Ndarray(int dims[])
     {
         shape = new int[dims.length];
-        num_inds=1;
+        multipliers = new int[dims.length];
+        int num_inds=1, dimsize=1;
         for(int i = 0; i < dims.length; i++)
         {
             shape[i] = dims[i];
             num_inds *= dims[i];
+        }
+        for(int i=dims.length - 1; i >= 0; i--)
+        {
+            multipliers[i] = dimsize;
+            dimsize *= dims[i];
         }
         arr = new double[num_inds];
         Random gen = new Random();
@@ -27,6 +33,29 @@ class Ndarray
     {
         for(int i = 0; i < num_inds; i++)
             arr[i] = x;
+    }
+
+    private int find_index(int idx[])
+    {
+        int internal_idx = 0;
+        for(int i = 0; i < idx.length; i++)
+        {
+            internal_idx = multipliers[i] * idx[i];
+        }
+
+        return internal_idx;
+    }
+    
+    public double get(int idx[])
+    {
+        int index = find_index(idx);
+        return arr[index];
+    }
+
+    public void put(int idx[], double val)
+    {
+        int index = find_index(idx);
+        arr[index] = val;
     }
 
     public Ndarray dot(Ndarray a, Ndarray b)
@@ -51,6 +80,46 @@ class Ndarray
             }
         }
 
+        return res;
+    
+    }
 
+    public Ndarray elementp(Ndarray a, Ndarray b)
+    {
+        if(a.shape.length != b.shape.length)
+            throw new IllegalArgumentException();
+        int n = a.num_inds;
+        for(int i = 0; i< a.shape.length; i++)
+        {
+            if(a.shape[i] != b.shape[i])
+                throw new IllegalArgumentException();
+        }
+        Ndarray res = new Ndarray(a.shape);
+        for(int i = 0; i < n; i++)
+            res.put_linear(i, a.get_linear(i) * b.get_linear(i));  
+    }
+
+    public void reshape(int new_shape[])
+    {
+        int total = 1;
+        for(int i = 0; i < new_shape.length; i++)
+        {
+            total *= new_shape[i];
+        }
+        if(total != num_inds)
+            throw new IllegalArgumentException();
+        shape = new int[new_shape.length];
+        multipliers = new int[new_shape.length];
+        int dims = 1;
+        for(int i = 0; i < shape.length; i++)
+        {
+            shape[i] = new_shape[i];
+        }
+
+        for(int i = multipliers.length - 1; i >= 0; i--)
+        {
+            multipliers[i] = dims;
+            dims *= new_shape[i];
+        }
     }
 }
